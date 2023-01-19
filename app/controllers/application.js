772e1431c854a4881835a1d84e5e707f167672ba
shalinivanan01 <chalenevanan1@gmail.com>
2023-01-19 12:52:36 +0530

    removed interaction with DOM

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,12 +2,13 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { A } from '@ember/array';
 export default Controller.extend({
+  userName:null,
   firstName: 'user',
   lastName: null,
   fullName: computed('firstName', 'lastName', function () {
-    console.log(this.firstName);
     return `${this.firstName} ${this.lastName}`;
   }),
+  itemValue:null,
   categories: A([
     {
       name: 'Grocery',
@@ -24,8 +25,7 @@ export default Controller.extend({
   ]),
   actions: {
     setUserName() {
-      let userName = document.getElementById('userName');
-      let [firstName, lastName] = userName.value.split(/\s+/);
+      let [firstName, lastName] = this.userName.split(/\s+/);
       this.set('firstName', firstName);
       this.set('lastName', lastName);
     },
